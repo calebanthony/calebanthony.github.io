d6bdf7c345610b43eb9cd04836184586fa578667
calebanthony <caleb.m.anthony@gmail.com>
2018-10-15 13:03:01 -0600

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * Expand and hide the menu on mobile
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+        const $target = document.getElementById(el.dataset.target);
+
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
